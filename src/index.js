0bae2f9f02be35ef039aac4b72ca356563391684
marvall <yariknarik.xxx@gmail.com>
2021-01-10 14:50:37 +0200

    add intersection observer

--- src/index.js
+++ src/index.js
@@ -3,33 +3,48 @@ import API from './js/apiService';
 import makeImageCard from './templates/imageCard.hbs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'material-design-icons/iconfont/material-icons.css';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.css';
+require('intersection-observer');
 
+//рефералки
 const refGallery = document.querySelector('.gallery');
 const refForm = document.querySelector('.search-form');
 const refButtonLoad = document.querySelector('.load-more');
+//const refLightBox = document.querySelector('.basicLightbox');
 let flagNewPage = 1;
+let viewElement;
 
+// Загрузка картинок
 const loadImages = (item, flag) => {
   return API.getImages(item, flag).then(data => {
     refGallery.insertAdjacentHTML('beforeend', makeImageCard(data.hits));
   });
 };
-const observer = e => {
+
+//Слушатель событий
+const observerWatch = e => {
   e.preventDefault();
 
   if (e.target.id === 'search-form') {
     refGallery.innerHTML = '';
-    loadImages(e.target.firstElementChild.value, flagNewPage).then(() => {
-      window.scrollTo({
-        top: 60,
-        behavior: 'smooth',
-      });
-    });
     flagNewPage = 1;
+    return loadImages(e.target.firstElementChild.value, flagNewPage)
+      .then(() => {
+        window.scrollTo({
+          top: 60,
+          behavior: 'smooth',
+        });
+      })
+      .then(() => {
+        viewElement = document.querySelector('.gallery').lastElementChild;
+        console.log(viewElement);
+        lazyLoad(viewElement);
+      });
   } else if ((e.target.id = 'load-more')) {
     flagNewPage += 1;
     let count = document.querySelector('.gallery').clientHeight + 60;
-    loadImages(refForm.firstElementChild.value, flagNewPage).then(() => {
+    return loadImages(refForm.firstElementChild.value, flagNewPage).then(() => {
       window.scrollTo({
         top: count,
         behavior: 'smooth',
@@ -37,5 +52,40 @@ const observer = e => {
     });
   }
 };
-refForm.addEventListener('submit', observer);
-refButtonLoad.addEventListener('click', observer);
+
+const showLargeImages = e => {
+  e.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.large}" width="800">
+`);
+
+  instance.show();
+};
+
+/* test для intersection */
+
+const lazyLoad = entry => {
+  flagNewPage += 1;
+  console.log(flagNewPage);
+  const io = new IntersectionObserver(
+    (entry, observer) => {
+      if (entry[0].isIntersecting) {
+        loadImages(refForm.firstElementChild.value, flagNewPage).then(() => {
+          viewElement = document.querySelector('.gallery').lastElementChild;
+          lazyLoad(viewElement);
+        });
+        observer.disconnect();
+      }
+    },
+    {
+      threshold: 1,
+      rootMargin: '0px',
+    },
+  );
+  io.observe(entry);
+};
+
+//eventListeners
+refForm.addEventListener('submit', observerWatch);
+refButtonLoad.addEventListener('click', observerWatch);
+refGallery.addEventListener('click', showLargeImages);
